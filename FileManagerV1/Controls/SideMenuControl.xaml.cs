a6a98cd35d4e59f46cba6c9a8789d121ffc3c96c
Zaqn <amirulzaquan@yahoo.com>
2019-06-16 23:17:13 +0800

    Add Directory Tree

---
+++ FileManagerV1/Controls/SideMenuControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileManagerV1
+{
+    /// <summary>
+    /// Interaction logic for SideMenuControl.xaml
+    /// </summary>
+    public partial class SideMenuControl : UserControl
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Defalut Constructor
+        /// </summary>
+        public SideMenuControl()
+        {
+            InitializeComponent();
+            
+        }
+        #endregion
+
+        #region On Loaded
+        /// <summary>
+        /// When the application first open
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                F
+            }
+        }
+        #endregion
+    }
+}
