1d07d8dfb4277f782f2810b5de4be666364f41e6
Zaqn <amirulzaquan@yahoo.com>
2019-06-18 00:01:22 +0800

    Display Side menu directory properly

---
+++ FileManagerV1/SideMenuDir/HeaderToImageConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace FileManagerV1.SideMenuDir
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+
+            if (path == null)
+                return null;
+            var name = SideMenuControl.GetFileFolderName(path);
+            var image = "Images/Directory/FileIcon.png";
+
+            if (string.IsNullOrEmpty(name))
+                image = "Images/Directory/DriveIcon.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/Directory/FolderIcon.png";
+
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
